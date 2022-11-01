8d44a269d25b036e350806022f3770577a195710
KATHLEEN FERREIRA DOS SANTOS SILVA <90014122+Kathleenfs@users.noreply.github.com>
2022-11-01 20:01:52 -0300

    deletar e cadastrar clientes criado

--- service/cliente-service.js
+++ service/cliente-service.js
@@ -1,9 +1,34 @@
-const listaCliente = () => {
-    return fetch('http://localhost:3000/profile')
+const listaClientes = () => {
+    return fetch(`http://localhost:3000/profile`)
         .then(resposta => {
             return resposta.json()
         })
 }
+
+const criaCliente = (nome, email) => {
+    return fetch(`http://localhost:3000/profile`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            nome: nome,
+            email: email
+        })
+    })
+        .then(resposta => {
+            return resposta.body
+        })
+}
+
+const removeCliente = (id) => {
+    return fetch(`http://localhost:3000/profile/${id}`, {
+        method: 'DELETE'
+    })
+}
+
 export const clienteService = {
-    listaCliente
+    listaClientes,
+    criaCliente,
+    removeCliente
 }
